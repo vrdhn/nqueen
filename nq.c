c9d4a984e245b7afddf0d0c49c4893fd4a9bcff8
Vardhan <vardhanvarma@gmail.com>
2016-08-16 09:09:32 +0530

    License

--- nq.c
+++ nq.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-
-
+/** (C) Vardhan Varma <vardhanvarma@gmail.com 
+ ** License: GPL v3 or later.
+ **
+ **   Should be one of the smallest solution to n-queen problem.
+ ** TODO:
+ **   (1) is the checking function correct ?
+ **   (2) Find number magic to detect transformations
+ */
 
 int chk(int ar[8])
 {
@@ -19,6 +25,7 @@ int chk(int ar[8])
 
 int  main () {
     int arr[8];
+    int count = 0;
     for ( long n = 0 ; n < 077777777 ; n ++ ) {
 	arr[0] = ( n >> 0 )  & 0x7;
 	arr[1] = ( n >> 3 )  & 0x7;
@@ -29,15 +36,18 @@ int  main () {
 	arr[6] = ( n >> 18 )  & 0x7;
 	arr[7] = ( n >> 21 )  & 0x7;
 	if ( chk(arr) )  {
-	    printf("==== %8.8o ==== \n",n);
-	    for ( int j = 0 ; j < 8 ; j ++ ) {
-		for ( int k = 0 ; k < arr[j] ; k ++ ) putchar('.');
-		putchar('Q');
-		for( int k = arr[j] + 1 ; k < 8 ; k ++ ) putchar('.');
-		putchar('\n');
+	    count ++;
+	    if ( 0 ) { 
+		printf("==== %8.8o ==== \n",n);
+		for ( int j = 0 ; j < 8 ; j ++ ) {
+		    for ( int k = 0 ; k < arr[j] ; k ++ ) putchar('.');
+		    putchar('Q');
+		    for( int k = arr[j] + 1 ; k < 8 ; k ++ ) putchar('.');
+		    putchar('\n');
+		}
+		printf("=============================\n");
 	    }
-
 	}
     }
-
+    printf("Solutions Found: %d\n",count);
 }
